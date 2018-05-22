b9801aeb142b6148ac4d4b612e6a320d0789623a
RonJohnC <38567661+RonJohnC@users.noreply.github.com>
2018-05-22 13:32:29 -0400

    Starting Javascript practice

---
+++ Javascript Practice/calculatorScript.js
@@ -0,0 +1,8 @@
+var ageYears = prompt ("How old are you?");
+var age = ageYears * 365.25;
+
+
+document.write ("You have been alive for " + age + " days.");
+
+console.log ("ageYears " + ageYears);
+console.log ("age " + age);
---
+++ Javascript Practice/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt ("What is your first name?");
+var lastName = prompt ("What is your last name?");
+var age = prompt ("How old are you?");
+
+console.log ("Hello " + firstName + " " + lastName + ", how nice to make your acquaintance.")
+console.log ("Hard to believe you are only " + age + " years old")
