8c46660942043c0f18c2a5809e6ecce1b2c480ca
RonJohnC <38567661+RonJohnC@users.noreply.github.com>
2018-05-30 16:11:28 -0400

    doing for loop javascript practice

---
+++ Javascript Practice/forLoop.js
@@ -0,0 +1,41 @@
+// var number = -10;
+// var even = 10;
+// var odd = 301;
+// var counter = 5
+
+// while (number <= 19) {
+// 	document.write(number);
+// 	number++;
+// }
+ for (i = -10; i <= 19; i++) {
+ 	console.log(i);
+ }
+
+// while (even <= 40){
+// 	document.write(even);
+// 	even+=2;
+// }
+
+for (i = 10; i <= 40; i+=2) {
+	console.log(i);
+}
+
+// while (odd <= 333){
+// 	document.write(" " + odd);
+// 	odd+=2;
+// }
+
+for (i=301; i <= 333; i+=2) {
+	console.log(i);
+}
+
+// while ( counter <= 50) {
+// 	if (counter % 3 === 0 && counter % 5 === 0){
+// 		document.write("  " + counter);
+// 	}
+// 	counter+=1;
+// }
+
+for (i=15; i<=50; )	{
+	console.log(i);
+}
